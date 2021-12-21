57576eac71de6fdbd62f5841b97c07217283be74
Erik <000erik001@gmail.com>
2021-12-21 16:52:12 +0400

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. In porro mollitia voluptate molestiae fugiat dicta a cumque ducimus voluptatum cupiditate eligendi consequatur numquam amet, recusandae suscipit debitis, molestias est ab?
+        Amet repellat asperiores recusandae maiores, eaque, ipsam consectetur beatae praesentium obcaecati magnam necessitatibus? Commodi omnis reiciendis natus. Esse obcaecati facere labore quia molestias sapiente vitae nemo adipisci totam voluptates. Nisi.
+      </p>
+    </section>
+  )
+}
+
+export default About
---
+++ src/pages/Error.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Error = () => {
+  return (
+    <section className="error-page section">
+      <div className="error-container">
+        <h1>oops! it's a dead end</h1>
+        <Link to="/" className='btn btn-primary'>Back Home</Link>
+     </div>
+     <h2>error page</h2>
+    </section>
+  )
+}
+
+export default Error
